afd15a6dbf32728da3059d096f9d7fe807236ae8
Amritapa Mondal <amritapamondal@gmail.com>
2024-09-17 01:15:09 +0530

    corrected import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import CreateBooks from "./pages/CreateBooks";
 import ShowBook from "./pages/ShowBook";
